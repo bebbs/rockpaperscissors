0ac06c1b04be694fabe485aa5f6b2e8caf8b9c62
Josh Bebbington <josh@focalwebsites.com>
2015-01-09 15:15:26 +0000

    Created player, initialised with a name

---
+++ spec/player_spec.rb
@@ -0,0 +1,15 @@
+require 'player'
+
+describe Player do
+
+  let(:player){Player.new('Josh')}
+
+  
+  context 'A player can' do
+    it 'have a name' do
+      expect(player.name).to eq 'Josh'
+    end
+
+  end
+
+end
