6fd78b9a0f285d43b3246bfd3b70a23752b5ba15
Josh Bebbington <josh@focalwebsites.com>
2015-01-09 12:23:26 +0000

    Added basic game logic to return a random response

--- lib/game_engine.rb
+++ lib/game_engine.rb
@@ -1,2 +1,15 @@
 class GameEngine
+
+  def chance
+    rand(1..3)
+  end
+
+  def response
+    case chance
+      when 1 then "Rock"
+      when 2 then "Paper"
+      when 3 then "Scissors"
+    end
+  end
+
 end
--- spec/game_engine_spec.rb
+++ spec/game_engine_spec.rb
@@ -1,5 +1,24 @@
 require 'game_engine'
 
-describe game_engine do
-  
+describe GameEngine do
+
+  let(:game){GameEngine.new}
+
+  context 'Can return'
+
+  it 'rock' do
+    allow(game).to receive(:chance).and_return(1)
+    expect(game.response).to eq("Rock")
+  end
+
+  it 'paper' do
+    allow(game).to receive(:chance).and_return(2)
+    expect(game.response).to eq("Paper")
+  end
+
+  it 'scissors' do
+    allow(game).to receive(:chance).and_return(3)
+    expect(game.response).to eq("Scissors")
+  end
+
 end
