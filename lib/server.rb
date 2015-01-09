42d172c629498d131ab4bb1ca884ab9dcd530d4c
Josh Bebbington <josh@focalwebsites.com>
2015-01-09 17:46:38 +0000

    Players can register and have a session created for them

--- lib/server.rb
+++ lib/server.rb
@@ -3,8 +3,25 @@ require_relative 'game_engine'
 require_relative 'player'
 
 class RockPaperScissors < Sinatra::Base
+
+  set :views, Proc.new {File.join(root, '..', 'views')}
+  enable :sessions
+  
+  game = GameEngine.new
+
   get '/' do
     erb :index
+  end
+
+  post '/register' do
+    @player = Player.new(params[:name])
+    session[:player] = @player
+    redirect('/play')
+  end
+
+  get '/play' do
+    @player = session[:player]
+    erb :play
   end
 
   # start the server if ruby file executed directly
