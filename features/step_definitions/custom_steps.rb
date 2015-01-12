e605d96483ca9674e8efcda4f01a8508f689dbf4
Josh Bebbington <josh@focalwebsites.com>
2015-01-12 13:53:32 +0000

    Fixed tests

--- features/step_definitions/custom_steps.rb
+++ features/step_definitions/custom_steps.rb
@@ -5,13 +5,13 @@ Given(/^I have registered as a player$/) do
 end
 
 Then(/^I should see the rock, paper, and scissor buttons$/) do
-  page.should have_selector('a', text: 'Rock')
-  page.should have_selector('a', text: 'Paper')
-  page.should have_selector('a', text: 'Scissors')
+  page.should have_selector('a', text: 'ROCK')
+  page.should have_selector('a', text: 'PAPER')
+  page.should have_selector('a', text: 'SCISSORS')
 end
 
 Given(/^I choose rock$/) do
-  click_link('Rock')
+  click_link('ROCK')
 end
 
 Then(/^I should see a result$/) do
@@ -26,7 +26,7 @@ Given(/^I have played one game$/) do
 end
 
 Then(/^I should see the game page$/) do
-  page.should have_content('Rock')
-  page.should have_content('Paper')
-  page.should have_content('Scissors')
+  page.should have_content('ROCK')
+  page.should have_content('PAPER')
+  page.should have_content('SCISSORS')
 end
